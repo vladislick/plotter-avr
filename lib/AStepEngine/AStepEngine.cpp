088de097013cd7260ea67bf74b12ffc0573fbe8e
vladislick <vladislickness@gmail.com>
2019-01-05 00:48:09 +0700

    Added half step mode

--- lib/AStepEngine/AStepEngine.cpp
+++ lib/AStepEngine/AStepEngine.cpp
@@ -53,7 +53,7 @@ void AStepEngine::step(EngineDir direction) {
       if (currentStep > 0) currentStep--;
       else currentStep = 3;
     }
-    /* Даём i реальный номер пина */
+    /* Даём d реальный номер пина */
     if (engineType == BIPOLAR) {
       if (currentStep == 0) d = 0;
       else if (currentStep == 1) d = 2;
@@ -68,10 +68,36 @@ void AStepEngine::step(EngineDir direction) {
   }
   /* ------- ПОЛУШАГОВЫЙ РЕЖИМ ------- */
   else if (engineMode == HALF) {
+    /* Переключаем текущий шаг */
     if (direction == FORWARD) {
-
+      if (currentStep < 7) currentStep++;
+      else currentStep = 0;
     } else {
-
+      if (currentStep > 0) currentStep--;
+      else currentStep = 7;
     }
+    /* Даём d реальные номера пинов */
+    if (currentStep == 0)       d = 0b0001;
+    else if (currentStep == 1)  d = 0b0011;
+    else if (currentStep == 2)  d = 0b0010;
+    else if (currentStep == 3)  d = 0b0110;
+    else if (currentStep == 4)  d = 0b0100;
+    else if (currentStep == 5)  d = 0b1100;
+    else if (currentStep == 6)  d = 0b1000;
+    else if (currentStep == 7)  d = 0b1001;
+
+    /* Включаем нужные пины */
+    for (uint8_t i = 0; i < 4; i++) {
+      if (d & (1 << i)) {
+        if (i == 0) *port |= (1 << pins[0]);
+        else if (i == 1) *port |= (1 << pins[2]);
+        else if (i == 2) *port |= (1 << pins[1]);
+        else if (i == 3) *port |= (1 << pins[3]);
+      }
+    }
+
+    /* Выключаем пины */
+    _delay_ms_fix(stepTime);
+    for (uint8_t i = 0; i < 4; i++) *port &= ~(1 << pins[i]);
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -28,20 +28,22 @@ int main() {
   LED_INIT();
   serial.begin(9600);
   servo.attach(&PORTB, 6);
-  engine1.attach(STANDART, &PORTC, 0, 1, 2, 3);
-  engine2.attach(STANDART, &PORTD, 7, 6, 5, 4);
+  engine1.attach(HALF, &PORTC, 0, 1, 2, 3);
+  engine2.attach(HALF, &PORTD, 7, 6, 5, 4);
+  engine1.setStepTime(4);
+  engine2.setStepTime(4);
 
   /* Разрешаем глобалные прерывания */
   sei();
 
   while(1) {
     servo.write(120);
-    for (uint8_t i = 0; i < 50; i++) engine1.step(FORWARD);
-    for (uint8_t i = 0; i < 50; i++) engine2.step(FORWARD);
+    for (uint16_t i = 0; i < 320; i++) engine1.step(FORWARD);
+    for (uint16_t i = 0; i < 480; i++) engine2.step(FORWARD);
     _delay_ms(100);
     servo.write(60);
-    for (uint8_t i = 0; i < 50; i++) engine1.step(BACKWARD);
-    for (uint8_t i = 0; i < 50; i++) engine2.step(BACKWARD);
+    for (uint16_t i = 0; i < 320; i++) engine1.step(BACKWARD);
+    for (uint16_t i = 0; i < 480; i++) engine2.step(BACKWARD);
     _delay_ms(100);
   }
 }
