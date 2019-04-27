a731cd51eca72c3afdfb8a0eb8cda899de7dad0a
Vladislav <vladislickness@gmail.com>
2019-04-28 00:04:14 +0700

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,8 @@
 #define   LED_YELLOW_ON()    PORTC |= (1 << 5)
 
 /* Максимальное количество шагов */
-#define   ENGINEX_MAX 240
-#define   ENGINEY_MAX 160
+#define   ENGINEX_MAX 480
+#define   ENGINEY_MAX 320
 
 /* Создаём все объекты для работы оборудования */
 ASerial       serial(true);       //Последовательный порт
@@ -33,8 +33,9 @@ AServo        servo(600, 2800);   //Cервопривод
 AStepEngine   engineX(BIPOLAR);   //Первый шаговый двигатель
 AStepEngine   engineY(BIPOLAR);   //Второй шаговый двигатель
 
-volatile char led_index1, //Для увеличения задержки таймера 0
-              led_index2; //Для увеличения задержки таймера 2
+volatile char led_index1,       //Для увеличения задержки таймера 0
+              led_index2;       //Для увеличения задержки таймера 2
+bool          overheating = 0;  //Показывает, включен ли режим охлаждения
 
 /* Возвращает модуль числа num */
 inline int16_t abs(int16_t num) {
@@ -44,7 +45,7 @@ inline int16_t abs(int16_t num) {
 
 /* Выключаем жёлтый светодиод по таймеру */
 ISR(TIMER0_OVF_vect) {
-  if (led_index1 > 3) {
+  if (led_index1 > 2) {
     LED_YELLOW_OFF();
     TCCR0 = 0;
   } else led_index1++;
@@ -52,7 +53,7 @@ ISR(TIMER0_OVF_vect) {
 
 /* Мигаем зелёным светодиодом по таймеру */
 ISR(TIMER2_OVF_vect) {
-  if (led_index2 > 30) {
+  if (led_index2 > (25 - (overheating * 22))) {
 
     if (LED_GREEN_IS_ON()) {
       LED_GREEN_OFF();
@@ -70,21 +71,25 @@ int main() {
   char      data;
   uint8_t   index = 0;
   uint8_t   mode;
-  uint16_t  value[5]; //Хранит числовые значения команд (координаты, номер команды)
-  bool      past[5];
+  uint16_t  value[7]; //Хранит числовые значения команд (координаты, номер команды)
+  bool      past[7];
+  uint8_t   angle = 40;
 
-  /* Предыдущее значение сервопривода */
-  uint8_t servo_last = 0;
+  /* Переменные для режима охлаждения */
+  uint16_t  stepcounter;
+  uint16_t  overheatvalue;
+  uint16_t  heatingtime;
 
   /* Инициализация оборудования станка */
   LED_GREEN_INIT();
   LED_YELLOW_INIT();
   serial.begin(9600);
   servo.attach(&PORTB, 4);
+  servo.write(37);
   engineX.attach(STANDART, &PORTC, 0, 1, 2, 3);
   engineY.attach(STANDART, &PORTB, 0, 1, 2, 3);
-  engineX.setStepTime(3);
-  engineY.setStepTime(3);
+  engineX.setStepTime(15);
+  engineY.setStepTime(15);
   engineX.setCoordinate(0);
   engineY.setCoordinate(0);
 
@@ -113,7 +118,7 @@ int main() {
       data = serial.read();
       if (data == '\0' || data == '\n') {
         /* Распознаём команду */
-        past[0] = past[1] = past[2] = past[3] = false;
+        past[0] = past[1] = past[2] = past[3] = past[4] = past[5] = past[6] = false;
         for (uint8_t i = 0; i < index; i++) {
           if (str[i] == 'G') {
             mode = 0;
@@ -122,19 +127,27 @@ int main() {
           } else if (str[i] == 'X') {
             mode = 1;
             value[mode] = 0;
-            past[1] = true;
+            past[mode] = true;
           } else if (str[i] == 'Y') {
             mode = 2;
             value[mode] = 0;
-            past[2] = true;
+            past[mode] = true;
           } else if (str[i] == 'Z') {
             mode = 3;
             value[mode] = 0;
-            past[3] = true;
+            past[mode] = true;
           } else if (str[i] == 'F') {
             mode = 4;
             value[mode] = 0;
-            past[4] = true;
+            past[mode] = true;
+          } else if (str[i] == 'H') {
+            mode = 5;
+            value[mode] = 0;
+            past[mode] = true;
+          } else if (str[i] == 'T') {
+            mode = 6;
+            value[mode] = 0;
+            past[mode] = true;
           } else if (str[i] > 47 && str[i] < 58) {
             value[mode] = (value[mode] * 10) + (str[i] - 48);
           }
@@ -151,32 +164,71 @@ int main() {
 
           /* Если это команда включения режима удержания позиции */
           if (value[0] == 1) {
-            //engineX.setPositionHolding(1);
+            engineX.setPositionHolding(1);
             engineY.setPositionHolding(1);
           }
           /* Если это команда выключения режима удержания позиции */
           else if (value[0] == 2) {
-            //engineX.setPositionHolding(0);
+            engineX.setPositionHolding(0);
             engineY.setPositionHolding(0);
+          }
+
+          /* Если нужно изменить режим охлаждения */
+          if (past[5]) {
+            overheatvalue = value[5];
+            heatingtime   = 1000;
+            stepcounter   = 0;
+          }
+
+          /* Если нужно изменить время охлаждения */
+          if (past[6]) {
+            heatingtime   = value[6];
           }
 
           /* Если нужно изменить скорость */
           if (past[4] && value[4] > 0 && value[4] < 11) {
-            engineX.setStepTime(12 - value[4]);
-            engineY.setStepTime(12 - value[4]);
+            engineX.setStepTime(20 - value[4]);
+            engineY.setStepTime(20 - value[4]);
           }
+
           /* Если нужно поменять текущее значение оси Z */
           if (past[3]) {
             servo.write(value[3]);
-            _delay_ms_fix(260 + engineX.getStepTime() * 20);
+            _delay_ms_fix(abs(angle - value[3]) * 15);
+            angle = value[3];
+            _delay_ms(250);
+            //Если включен режим охлаждения
+            if (overheatvalue > 0 && stepcounter > overheatvalue) {
+              //Включаем светодиод на мигание
+              overheating = 1;
+              LED_GREEN_OFF();
+              TCCR2 = (1 << CS22)|(1 << CS21)|(1 << CS20);
+              //Делаем задержку для охлаждения
+              _delay_ms_fix(heatingtime);
+              stepcounter = 0;
+              //Возвращаем светодиод в обычное состояние
+              TCCR2 = 0;
+              TCNT2 = 0;
+              led_index2 = 0;
+              overheating = 0;
+              LED_GREEN_ON();
+            }
           }
+
           /* Если нужно поменять текущее значение оси X */
           if (past[1]) if (value[1] <= ENGINEX_MAX) {
+            if (overheatvalue > 0) {
+              stepcounter += abs(engineX.getCoordinate() - value[1]);
+            }
             engineX.move(value[1]);
             _delay_ms(10);
           }
+
           /* Если нужно поменять текущее значение оси Y */
           if (past[2]) if (value[2] <= ENGINEY_MAX) {
+            if (overheatvalue > 0) {
+              stepcounter += abs(engineY.getCoordinate() - value[2]);
+            }
             engineY.move(value[2]);
             _delay_ms(10);
           }
